cc72a227b270a2aa11c937a927303853b7cb35d9
Dillon <d.vorav612@gmail.com>
2021-03-19 03:14:03 -0500

    Added install to service

--- public/service.worker.js
+++ public/service.worker.js
@@ -7,3 +7,19 @@ const FILES_TO_CACHE = [
 "/icons/icon-192x192.png",
 "/icons/icon-512x512.png"
 ];
+
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+//install step 
+
+self.addEventListener("install", function (event) {
+event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+    })
+)
+self.skipWaiting();
+
+})
