a04a4cf82629692ebd159b00b59be97a84f8dace
Dillon <d.vorav612@gmail.com>
2021-03-19 03:01:55 -0500

    db.js update

--- public/db.js
+++ public/db.js
@@ -6,7 +6,7 @@ request.onupgradeneeded = function(event) {
 
     const db = event.target.result;
     db.createObjectStore("pending", {autoIncrement: true});
-}
+};
 
 request.onsuccess = function(event) {
     db = event.target.result;
@@ -14,9 +14,45 @@ request.onsuccess = function(event) {
     if (navigator.onLine) {
         checkDatabase();
     }
-}
+};
 
 request.onerror = function(event) {
 console.log("ERROR" + event.target.console.errorCode);
 
 };
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore('pending');
+    store.add(record)
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+    
+    const getAll = store.getAll();
+
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+fetch("/api/transaction/bulk", {
+    method: "POST",
+    body: JSON.stringify(getAll.result),
+    headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+    }
+})
+.then(response = response.json())
+.then(() => {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.clear()
+})
+        }
+    }
+
+}
