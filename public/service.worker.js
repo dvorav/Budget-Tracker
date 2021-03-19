3355782b58bd69765da06995d78a9be26061dd78
Dillon <d.vorav612@gmail.com>
2021-03-19 03:46:27 -0500

    fix service worker??

--- public/service.worker.js
+++ public/service.worker.js
@@ -1,48 +1,49 @@
 const FILES_TO_CACHE = [
-
-  "/",
-  "/index.html",
-  "/index.js",
-  "/flavicon.ico",
-  "/styles.css",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
-];
-
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
-//install step
-
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
-});
-
-//activate
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    cacheskeys().then((keyList) => {
-      return Promise.all(
-        keyLlist.map((key) => {
-          if (key != CACHE_NAME && key !== DATA_CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-//fetch 
-
-// fetch
-self.addEventListener("fetch", function (evt) {
+    "/", 
+    "/index.html", 
+    "/index.js", 
+    "/favicon.ico", 
+    "/styles.css", 
+    "/icons/icon-144x144.png",
+    "/icons/icon-192x192.png", 
+    "/icons/icon-512x512.png"
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  // install
+  self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  // activate
+  self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function (evt) {
     if (evt.request.url.includes("/api/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
