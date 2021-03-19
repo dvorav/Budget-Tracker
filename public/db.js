36c8e98e4590fc3095416bfaea337b0bb87510ee
Dillon Voravongsy <dillonvoravongsy@Dillons-MBP-2.domain>
2021-03-19 02:53:04 -0500

    Added success and error function to db.js

--- public/db.js
+++ public/db.js
@@ -7,3 +7,16 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore("pending", {autoIncrement: true});
 }
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+}
+
+request.onerror = function(event) {
+console.log("ERROR" + event.target.console.errorCode);
+
+};
