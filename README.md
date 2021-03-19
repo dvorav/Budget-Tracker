d962c11bd7a2d17939c904b8f79bcf3bfded230e
Dillon <d.vorav612@gmail.com>
2021-03-19 05:31:15 -0500

    Finished commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Derp
+# Budget-Tracker
+
+
+## Description
+User will be to balance their budget and expenses and have it be saved either offline or online. Offline, you are able to input data, but once back online, data will then be stored on the database 
+
+
+## Deployed Link
+[Heroku Link](
+https://pwa-budget-trackerrr.herokuapp.com/)
+
+
+
+
+## Installation
+
+```bash
+npm i 
+```
+After installing dependencies:
+
+``` bash
+node server.js
+```
+
+## Visuals
+
+![frontpage](./public/images/page.PNG)
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Questions
+* Github: https://github.com/dvorav
+* Email: d.vorav612@gmail.com
--- public/service-worker.js
+++ public/service-worker.js
@@ -10,11 +10,10 @@ const FILES_TO_CACHE = [
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
   
-  // install
+  // Install
   self.addEventListener("install", function (evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
         return cache.addAll(FILES_TO_CACHE);
       })
     );
@@ -22,14 +21,13 @@ const FILES_TO_CACHE = [
     self.skipWaiting();
   });
   
-  // activate
+  // Activate
   self.addEventListener("activate", function (evt) {
     evt.waitUntil(
       caches.keys().then(keyList => {
         return Promise.all(
           keyList.map(key => {
             if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
               return caches.delete(key);
             }
           })
@@ -40,14 +38,13 @@ const FILES_TO_CACHE = [
     self.clients.claim();
   });
   
-  // fetch
+  // Fetch
   self.addEventListener("fetch", function (evt) {
     if (evt.request.url.includes("/api/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
             .then(response => {
-              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
@@ -55,7 +52,6 @@ const FILES_TO_CACHE = [
               return response;
             })
             .catch(err => {
-              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
         }).catch(err => console.log(err))
@@ -70,7 +66,6 @@ const FILES_TO_CACHE = [
           if (response) {
             return response;
           } else if (evt.request.headers.get("accept").includes("text/html")) {
-            // return the cached home page for all requests for html pages
             return caches.match("/");
           }
         });
