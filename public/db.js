daf2e1601587f75db627fbf45ef3dac1410657af
Dillon Voravongsy <dillonvoravongsy@Dillons-MBP-2.domain>
2021-03-19 02:49:47 -0500

    Added request function to db.js

---
+++ public/db.js
@@ -0,0 +1,9 @@
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+
+    const db = event.target.result;
+    db.createObjectStore("pending", {autoIncrement: true});
+}
